8d8fb6db72b0dc58fd55a5b11e8eef79536aad9b
tosida <you.golubitskaya@yandex.ru>
2024-10-25 08:59:07 +0300

    upd 25.10.24

--- cp/app.ts
+++ cp/app.ts
@@ -22,6 +22,7 @@ app.post("/encrypt", (req, res) => {
     const encryptedTextXXTEA = encrypt(req.body.enc_text, req.body.key);
     const buffer = util.createBuffer(encryptedTextXXTEA);
     const resultXXTEA = util.encode64(buffer.getBytes());
+    console.log(resultXXTEA);
     
     let endTime = performance.now();
     const encodingTimeXXTEA = (endTime - startTime).toFixed(4);
